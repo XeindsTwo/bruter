6410549ed1063e7cc0c77d60a9da2c5d5bfd2527
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-05-27 10:36:24 +0300

    Сделана секция главного экрана и шапка

--- app/js/main.js
+++ app/js/main.js
@@ -43,3 +43,22 @@ for (const anchor of anchors) {
   anchor.addEventListener('click', handleAnchorClick);
   anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
 }
+
+(function() {
+  const buttons = document.querySelectorAll(".header__btn");
+
+  buttons.forEach(button => {
+    ["mouseenter", "mouseout"].forEach(evt => {
+      button.addEventListener(evt, e => {
+        let parentOffset = button.getBoundingClientRect(),
+          relX = e.pageX - parentOffset.left,
+          relY = e.pageY - parentOffset.top;
+
+        const span = button.getElementsByTagName("span");
+
+        span[0].style.top = relY + "px";
+        span[0].style.left = relX + "px";
+      });
+    });
+  });
+})();
